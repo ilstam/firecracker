0b159cef1e119c4ff8ff8f0eed8c1bd73c65fe67
Ilias Stamatis <ilstam@amazon.com>
2026-05-13 17:28:11 +0100

    virtio: net: Implement device reset
    Implement the reset() method for the virtio-net device, resetting the
    net specific state in-place.
    
    Signed-off-by: Ilias Stamatis <ilstam@amazon.com>

--- src/vmm/src/devices/virtio/net/device.rs
+++ src/vmm/src/devices/virtio/net/device.rs
@@ -1073,6 +1073,12 @@ impl VirtioDevice for Net {
         self.device_state = DeviceState::Inactive;
     }
 
+    fn _reset(&mut self) -> bool {
+        self.rx_buffer.clear();
+        self.tx_buffer.clear();
+        true
+    }
+
     /// Prepare saving state
     fn prepare_save(&mut self) {
         // We shouldn't be messing with the queue if the device is not activated.
@@ -2533,5 +2539,17 @@ pub mod tests {
         let queues = net.queues();
         assert!(queues[RX_INDEX].uses_notif_suppression);
         assert!(queues[TX_INDEX].uses_notif_suppression);
+    }
+
+    #[test]
+    fn test_reset() {
+        let mem = single_region_mem(2 * MAX_BUFFER_SIZE);
+        let mut th = TestHelper::get_default(&mem);
+        th.activate_net();
+
+        assert!(th.net().is_activated());
+        assert!(th.net().reset());
+        assert!(!th.net().is_activated());
+        assert_eq!(th.net().acked_features(), 0);
     }
 }
